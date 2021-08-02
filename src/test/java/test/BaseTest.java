07137a55e9130c2de4be97bf2f51ae2d50f27d20
zotyo911 <sooszoltan79@gmail.com>
2021-08-02 17:07:01 +0200

    testDemo4

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,3 +1,5 @@
+package test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
--- src/test/java/test/WikiTest.java
+++ src/test/java/test/WikiTest.java
@@ -1,3 +1,5 @@
+package test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Allure;
 import org.junit.jupiter.api.*;
@@ -8,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.io.ByteArrayInputStream;
 import java.util.concurrent.TimeUnit;
 
-public class WikiTest extends BaseTest{
+public class WikiTest extends BaseTest {
 
 
 
