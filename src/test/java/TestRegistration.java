b571f1a01ce3e597b75aac9e52c31fed7599a9cf
zotyo911 <sooszoltan79@gmail.com>
2021-08-02 15:58:15 +0200

    testdemo2

--- src/test/java/TestRegistration.java
+++ src/test/java/TestRegistration.java
@@ -1,17 +1,49 @@
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import pages.LandingPage;
 
+import java.util.concurrent.TimeUnit;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestRegistration {
-    
+
+    WebDriver webdriver;
+
+    @BeforeEach
+    public void Init()
+    {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        webdriver = new ChromeDriver(options);
+
+        webdriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        webdriver.manage().window().maximize();
+
+    }
+
     @Test
+    @Order(1)
     public void testDemo(){
         LandingPage.clickHamburgerButton();
         LandingPage.clickLoginButton();
     }
 
     @Test
+    @Order(2)
     public void testDemo1(){
         LandingPage.clickHamburgerButton();
         LandingPage.clickTrainerButton();
+    }
+
+    @AfterEach
+    public void Close()
+    {
+        webdriver.quit();
     }
 }
